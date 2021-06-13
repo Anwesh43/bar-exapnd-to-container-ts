ad8c8925696b02392118ef22fc439b863801ea16
Anwesh43 <anweshthecool0@gmail.com>
2021-06-13 20:50:44 +0530

    adding BETCNode class

--- index.ts
+++ index.ts
@@ -60,7 +60,7 @@ class DrawingUtil {
         context.restore()
     }
 
-    static drawBECNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+    static drawBETCNode(context : CanvasRenderingContext2D, i : number, scale : number) {
         context.lineCap = 'round'
         context.lineWidth = Math.min(w, h) / strokeFactor 
         context.strokeStyle = colors[i]
@@ -141,5 +141,47 @@ class Animator {
             this.animated = false 
             clearInterval(this.interval)
         }
+    }
+}
+
+class BETCNode {
+
+    prev : BETCNode 
+    next : BETCNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BETCNode(this.i + 1)
+            this.next.prev = this  
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBETCNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : BETCNode {
+        var curr : BETCNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
     }
 }
