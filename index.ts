6c4c8729e8ebc3b45171250c1b646fe74fc97d2d
Anwesh43 <anweshthecool0@gmail.com>
2021-06-13 18:02:18 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -25,3 +25,46 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x1, y1)
+        context.stroke()
+    }
+
+    static drawBarExpandContainer(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts) 
+        context.save()
+        context.translate(w / 2 + (w / 2 + size) * sc4, h / 2)
+        context.rotate(rot * sc3)
+        for (var j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1, 1 - 2 * j)
+            for (var k = 0; k  < 2; k++) {
+                context.save()
+                context.scale(1 - 2 * k, 1)
+                context.translate((size) * j * sc2, 0)
+                DrawingUtil.drawLine(context, 0, size - size * sc1, 0, size)
+                context.restore()
+            }
+            context.restore()
+        }
+        context.fillRect(-size * sc2, -size, 2 * size * sc2, size)
+        context.restore()
+    }
+
+    static drawBECNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawBarExpandContainer(context, scale)
+    }
+}
